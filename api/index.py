e94e5bae441c45587b4d71a8a3e9a9d252447fc0
jadfost <stevenorduz@hotmail.com>
2023-11-14 18:58:51 -0500

    V1.3.0

--- api/index.py
+++ api/index.py
@@ -1,26 +1,26 @@
 import os
-import pandas as pd
+import csv
 from flask import Flask, render_template, jsonify, request
-
 
 app = Flask(__name__)
 
-# Cargar los datos desde los archivos CSV
-df1 = pd.read_csv('Datos/DIRS/DIRS 1.csv')
-df2 = pd.read_csv('Datos/DIRS/DIRS 2.csv')
-df3 = pd.read_csv('Datos/DIRS/DIRS 3.csv')
-df4 = pd.read_csv('Datos/DIRS/DIRS 4.csv')
+# Directorio que contiene los archivos CSV
+DIRS_DIR = 'Datos/DIRS/'
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Función para cargar datos desde archivos CSV
+def cargar_datos(tabla):
+    archivo_csv = os.path.join(DIRS_DIR, f'{tabla}.csv')
+    with open(archivo_csv, 'r') as file:
+        reader = csv.DictReader(file)
+        data = [row for row in reader]
+    return data
 
 # Ruta para manejar la solicitud de actualización del dropdown
 @app.route('/update_dropdown', methods=['POST'])
 def update_dropdown():
     tabla_seleccionada = request.form['tabla']
-    df_seleccionado = obtener_dataframe_segun_tabla(tabla_seleccionada)
-    columnas = df_seleccionado.columns
+    data_seleccionada = cargar_datos(tabla_seleccionada)
+    columnas = data_seleccionada[0].keys()
     opciones = [{'label': columna, 'value': columna} for columna in columnas]
     return jsonify({'columnas': opciones})
 
@@ -29,31 +29,31 @@ def update_dropdown():
 def update_graph():
     tabla_seleccionada = request.form['tabla']
     campo_seleccionado = request.form['campo']
-    df_seleccionado = obtener_dataframe_segun_tabla(tabla_seleccionada)
-    
-    # Verifica que la columna seleccionada esté en el DataFrame
-    if campo_seleccionado not in df_seleccionado.columns:
+    data_seleccionada = cargar_datos(tabla_seleccionada)
+
+    # Verifica que la columna seleccionada esté en los datos
+    if campo_seleccionado not in data_seleccionada[0]:
         return jsonify({'error': 'Columna no encontrada'})
 
     # Prepara los datos para el gráfico
     data = {
-        'x': df_seleccionado[campo_seleccionado].tolist(),
+        'x': [row[campo_seleccionado] for row in data_seleccionada],
         'type': 'bar',
         'marker': {'color': 'blue'}
     }
 
     return jsonify(data)
 
-# Función para obtener el DataFrame según la tabla seleccionada
-def obtener_dataframe_segun_tabla(tabla):
-    if tabla == 'DIRS 1':
-        return df1
-    elif tabla == 'DIRS 2':
-        return df2
-    elif tabla == 'DIRS 3':
-        return df3
-    elif tabla == 'DIRS 4':
-        return df4
-    
+# Función principal para obtener el DataFrame según la tabla seleccionada
+def obtener_datos_segun_tabla(tabla):
+    if tabla in ['DIRS 1', 'DIRS 2', 'DIRS 3', 'DIRS 4']:
+        return cargar_datos(tabla)
+    return []
+
+# Ruta para manejar la carga inicial de datos
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
