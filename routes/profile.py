f3af433c672ae802c5e4219940811cf370e5bfeb
Jared Foster <stevenorduz@hotmail.com>
2024-01-31 15:50:37 -0500

    V2.2.5 Main

--- routes/profile.py
+++ routes/profile.py
@@ -1,10 +1,22 @@
 # routes/profile.py
 from flask import Blueprint, render_template, redirect, url_for, session
+from routes.db.db import get_users_collection
 
 profile_bp = Blueprint('profile_bp', __name__)
+
+users_collection = get_users_collection()
 
 @profile_bp.route('/')
 def profile():
     if 'username' in session:
-        return render_template('profile.html')
+        print("Session username:", session['username'])
+        # Fetch user data from the database
+        user_data = users_collection.find_one({'username': session['username']})
+        print("User data:", user_data)
+
+        if user_data:
+            # Pass user data to the template
+            return render_template('profile.html', user=user_data)
+        
+    # Redirect to login if user data is not found or user is not logged in
     return redirect(url_for('auth_bp.login'))
