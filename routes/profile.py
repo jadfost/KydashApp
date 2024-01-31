c20ed91d48bda3e3c9fef8e6a061ef16834695ee
Jared Foster <stevenorduz@hotmail.com>
2024-01-31 16:19:57 -0500

    V2.2.6 Main

--- routes/profile.py
+++ routes/profile.py
@@ -1,5 +1,5 @@
 # routes/profile.py
-from flask import Blueprint, render_template, redirect, url_for, session
+from flask import Blueprint, render_template, redirect, url_for, session, request
 from routes.db.db import get_users_collection
 
 profile_bp = Blueprint('profile_bp', __name__)
@@ -9,14 +9,30 @@ users_collection = get_users_collection()
 @profile_bp.route('/')
 def profile():
     if 'username' in session:
-        print("Session username:", session['username'])
         # Fetch user data from the database
         user_data = users_collection.find_one({'username': session['username']})
-        print("User data:", user_data)
 
         if user_data:
             # Pass user data to the template
             return render_template('profile.html', user=user_data)
-        
+    
     # Redirect to login if user data is not found or user is not logged in
     return redirect(url_for('auth_bp.login'))
+
+@profile_bp.route('/edit_profile', methods=['POST'])
+def edit_profile():
+    if 'username' in session:
+        # Fetch user data from the database
+        user_data = users_collection.find_one({'username': session['username']})
+
+        if user_data:
+            # Update user information
+            user_data['name'] = request.form.get('new_name')
+            user_data['lastname'] = request.form.get('new_lastname')
+            user_data['email'] = request.form.get('new_email')
+
+            # Save the updated user data to the database
+            users_collection.update_one({'username': session['username']}, {'$set': user_data})
+
+    # Redirect to the profile page after updating
+    return redirect(url_for('profile_bp.profile'))
